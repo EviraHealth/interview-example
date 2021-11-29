56bd2072789a91cb238e08a5b02b162acac4e693
Love Marcus <love.marcus@outlook.com>
2021-11-29 20:47:57 +0100

    Css cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import logo from './logo.svg'
-import './App.css'
+import { backgroundGrey } from './color'
+import './index.css'
 
 function App () {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+    <div>
+      <header
+        style={{
+          backgroundColor: backgroundGrey,
+          display: 'flex',
+          height: '100vh',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img
+          src={logo}
+          style={{
+            height: '40vmin',
+            pointerEvents: 'none',
+            animation: 'App-logo-spin infinite 20s linear',
+          }}
+          alt='logo'
+        />
       </header>
     </div>
   )
---
+++ src/color.js
@@ -0,0 +1,5 @@
+module.exports = {
+  backgroundGrey: '#282c34',
+  turquoise: '#9CE7E2',
+  eviraBlue: '#223554',
+}
