4332113dacef7b2dd9e6fc8be18166052c60accc
Love Marcus <love.marcus@outlook.com>
2021-11-30 10:53:02 +0100

    Move app into index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import { backgroundGrey } from './color'
+import logo from './logo.svg'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <header
+        style={{
+          backgroundColor: backgroundGrey,
+          display: 'flex',
+          height: '100vh',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img
+          src={logo}
+          style={{
+            height: '40vmin',
+            pointerEvents: 'none',
+            animation: 'App-logo-spin infinite 20s linear',
+          }}
+          alt='logo'
+        />
+      </header>
+    </div>
   </React.StrictMode>,
   document.getElementById('root'),
 )
