1ec230341752801337539a6088d59ab69da2198f
Love Marcus <love.marcus@outlook.com>
2021-11-30 11:21:26 +0100

    Add a rudimentary chart

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import logo from './assets/logo.svg'
+import { Chart } from './Chart/Chart'
 import { backgroundGrey } from './color'
 import reportWebVitals from './reportWebVitals'
 
@@ -27,6 +28,7 @@ ReactDOM.render(
           }}
           alt='logo'
         />
+        <Chart />
       </header>
     </div>
   </React.StrictMode>,
